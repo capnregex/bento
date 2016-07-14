eea5fb694560dd3550f841899532ba3803911911
Jay Dorsey <me@github.jaydorsey.com>
2016-07-14 10:32:19 -0400

    Add rvm, chrome, node, etc

--- scripts/ubuntu/update.sh
+++ scripts/ubuntu/update.sh
@@ -15,13 +15,18 @@ sed -i.bak 's/^Prompt=.*$/Prompt=never/' /etc/update-manager/release-upgrades;
 # Update the package list
 apt-get -y update;
 
-# update package index on boot
-cat <<EOF >/etc/init/refresh-apt.conf;
-description "update package index"
-start on networking
-task
-exec /usr/bin/apt-get update
-EOF
+# Upgrade all installed packages incl. kernel and kernel headers
+apt-get -y dist-upgrade --force-yes;
+reboot;
+sleep 60;
+
+# # update package index on boot
+# cat <<EOF >/etc/init/refresh-apt.conf;
+# description "update package index"
+# start on networking
+# task
+# exec /usr/bin/apt-get update
+# EOF
 
 # Manage broken indexes on distro disc 12.04.5
 if [ "$ubuntu_version" = "12.04" ]; then
